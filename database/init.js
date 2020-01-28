cfa4cd7af9ffa154ded6968e219df0e863914076
Benjamin Pernick <bpernick@comcast.net>
2020-01-27 20:33:27 -0600

    remove module.exports from database.init

--- database/init.js
+++ database/init.js
@@ -9,77 +9,72 @@ var connection = mysql.createConnection({
   database: 'image_render'
 });
 
-
-module.exports.populate = () => {
-    fs.readFile (`/Users/ben/Desktop/ben_image_render/database/jsonData.json`, 'utf8', (err, data) => {
-        if (err) {
-            throw err;
-        } else {
-            let obj = JSON.parse(data.slice(1));
-            for ( let i = 0; i < obj.Sheet1.length; i++){
-                let entry = obj.Sheet1[i];
-                connection.query(`INSERT INTO products (name) VALUES (?)`, [entry.name], (err, msg) => {
+fs.readFile (`/Users/ben/Desktop/ben_image_render/database/jsonData.json`, 'utf8', (err, data) => {
+    if (err) {
+        throw err;
+    } else {
+        let obj = JSON.parse(data.slice(1));
+        for ( let i = 0; i < obj.Sheet1.length; i++){
+            let entry = obj.Sheet1[i];
+            connection.query(`INSERT INTO products (name) VALUES (?)`, [entry.name], (err, msg) => {
+                if (err){
+                    throw err;
+                }
+                console.log('WOOHOO!')
+            })
+            if (entry.image){
+                connection.query(`INSERT INTO images (image, product_id) VALUES (?,?)`, [entry.image, entry.id], (err, msg) => {
                     if (err){
                         throw err;
                     }
                     console.log('WOOHOO!')
                 })
-                if (entry.image){
-                    connection.query(`INSERT INTO images (image, product_id) VALUES (?,?)`, [entry.image, entry.id], (err, msg) => {
-                        if (err){
-                            throw err;
-                        }
-                        console.log('WOOHOO!')
-                    })
-                }
-
-                if (entry.extra_url1){
-                    connection.query(`INSERT INTO images (image, product_id) VALUES (?,?)`, [entry.extra_url1, entry.id], (err, msg) => {
-                        if (err){
-                            throw err;
-                        }
-                        console.log('WOOHOO!')
-                    })
-                }
-                if (entry.extra_url2){
-                    connection.query(`INSERT INTO images (image, product_id) VALUES (?,?)`, [entry.extra_url2, entry.id], (err, msg) => {
-                        if (err){
-                            throw err;
-                        }
-                        console.log('WOOHOO!')
-                    })
-                }
-
-                if (entry.extra_url3){
-                    connection.query(`INSERT INTO images (image, product_id) VALUES (?,?)`, [entry.extra_url3, entry.id], (err, msg) => {
-                        if (err){
-                            throw err;
-                        }
-                        console.log('WOOHOO!')
-                    })
-                }
-
-                if (entry.extra_url4){
-                    connection.query(`INSERT INTO images (image, product_id) VALUES (?,?)`, [entry.extra_url4, entry.id], (err, msg) => {
-                        if (err){
-                            throw err;
-                        }
-                        console.log('WOOHOO!')
-                    })
-                }
-
-                if (entry.extra_url5){
-                    connection.query(`INSERT INTO images (image, product_id) VALUES (?,?)`, [entry.extra_url5, entry.id], (err, msg) => {
-                        if (err){
-                            throw err;
-                        }
-                        console.log('WOOHOO!')
-                    })
-                }
             }
-            //connection.query(`INSERT INTO products`)
-        }
-    })
-}
 
-module.exports.populate ()
+            if (entry.extra_url1){
+                connection.query(`INSERT INTO images (image, product_id) VALUES (?,?)`, [entry.extra_url1, entry.id], (err, msg) => {
+                    if (err){
+                        throw err;
+                    }
+                    console.log('WOOHOO!')
+                })
+            }
+            if (entry.extra_url2){
+                connection.query(`INSERT INTO images (image, product_id) VALUES (?,?)`, [entry.extra_url2, entry.id], (err, msg) => {
+                    if (err){
+                        throw err;
+                    }
+                    console.log('WOOHOO!')
+                })
+            }
+
+            if (entry.extra_url3){
+                connection.query(`INSERT INTO images (image, product_id) VALUES (?,?)`, [entry.extra_url3, entry.id], (err, msg) => {
+                    if (err){
+                        throw err;
+                    }
+                    console.log('WOOHOO!')
+                })
+            }
+
+            if (entry.extra_url4){
+                connection.query(`INSERT INTO images (image, product_id) VALUES (?,?)`, [entry.extra_url4, entry.id], (err, msg) => {
+                    if (err){
+                        throw err;
+                    }
+                    console.log('WOOHOO!')
+                })
+            }
+
+            if (entry.extra_url5){
+                connection.query(`INSERT INTO images (image, product_id) VALUES (?,?)`, [entry.extra_url5, entry.id], (err, msg) => {
+                    if (err){
+                        throw err;
+                    }
+                    console.log('WOOHOO!')
+                })
+            }
+        }
+
+    }
+})
