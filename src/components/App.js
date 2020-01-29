49220f769f02ae5513ad6612f30c936a453e92aa
Benjamin Pernick <bpernick@comcast.net>
2020-01-29 12:23:04 -0600

    Render pictures correctly in position no resize

--- src/components/App.js
+++ src/components/App.js
@@ -30,8 +30,10 @@ export default class App extends Component {
       <div>
         {this.state.currentProduct && <Title className = 'ben' product = {this.state.currentProduct}/>}
         {this.state.currentProduct && <RatingsBar className = 'ben' product = {this.state.currentProduct}/>}
-        {this.state.currentProduct && <MainImage className = 'ben' product = {this.state.currentProduct}/>}
-        { this.state.currentProduct && <ImageBar className = 'ben' products = {this.state.products} product = {this.state.currentProduct}/>}
+        <div className = 'images'>
+          { this.state.currentProduct && <ImageBar className = 'ben' products = {this.state.products} product = {this.state.currentProduct}/>}
+          {this.state.currentProduct && <MainImage className = 'ben' product = {this.state.currentProduct}/>}
+        </div>
       </div>
     );
   }
